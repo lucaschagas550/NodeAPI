126452c203b14c7f4a4b5c7f69d3493afffb027d
Lucas Andrade <lucas.chagas@opuspacind.onmicrosoft.com>
2021-09-08 11:05:54 -0300

    Feat: Introdução e configuração do servidor.

--- src/app.js
+++ src/app.js
@@ -1,14 +1,8 @@
 'use strict' // faz vc lembrar de colocar virgula ou ate da erro caso não colocar
 
-const http = require('http');
-const debug = require('debug')('nodestr:server');
 const express = require('express');
 
 const app = express();
-const port = 3000;
-app.set('port', port);
-
-const server = http.createServer(app);
 const router = express.Router();
 
 const route = router.get('/', (req, res, next) => {
@@ -17,8 +11,6 @@ const route = router.get('/', (req, res, next) => {
         version: "0.0.1."
     });
 });
-
 app.use('/', route);
 
-server.listen(port);
-console.log("API rodando na porta " + port);
+module.exports = app;
